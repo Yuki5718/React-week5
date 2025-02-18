e5c19bb602e3a4b3a4c28f28c32e45b6e739c34d
yuki5718 <hayate8575@gmail.com>
2025-02-18 23:43:24 +0800

    新增套件：react-loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-
+import ReactLoading from 'react-loading';
 import Cart from './component/Cart';
 import ProductsList from './component/ProductsList';
 import OderForm from './component/OderForm';
